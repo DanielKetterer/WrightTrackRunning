f3a661e03ef5188e1c53797150fed916b28a9cbf
DanielKetterer <daniel.kette@gmail.com>
2021-02-26 18:53:30 -0500

    Update fps_demo.py

--- fps_demo.py
+++ fps_demo.py
@@ -18,22 +18,27 @@ args = vars(ap.parse_args())
 print("[INFO] sampling frames from webcam...")
 stream = cv2.VideoCapture(0)
 fps = FPS().start()
+
 # loop over some frames
 while fps._numFrames < args["num_frames"]:
 	# grab the frame from the stream and resize it to have a maximum
 	# width of 400 pixels
 	(grabbed, frame) = stream.read()
 	frame = imutils.resize(frame, width=400)
+	
 	# check to see if the frame should be displayed to our screen
 	if args["display"] > 0:
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
+		
 	# update the FPS counter
 	fps.update()
+	
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
 # do a bit of cleanup
 stream.release()
 cv2.destroyAllWindows()
@@ -43,7 +48,6 @@ cv2.destroyAllWindows()
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
-
 
 # loop over some frames...this time using the threaded stream
 while fps._numFrames < args["num_frames"]:
